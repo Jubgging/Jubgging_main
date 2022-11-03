098d51a803f6e1fdd9a36e17726049e284c9a9d5
sebin <cntpqls0218@gmail.com>
2022-11-04 03:05:13 +0900

    main_page 60%

--- app/src/main/java/com/example/myjubggingproject/MainActivity.kt
+++ app/src/main/java/com/example/myjubggingproject/MainActivity.kt
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myjubggingproject.databinding.ActivityMainBinding
 
+//메인 화면
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    //뷰가 생성되었을 때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root) //그릴 xml 뷰 파일을 연결 시켜준다.
 
         // 시작하기 버튼 누르면 화면 전환
         binding.startId.setOnClickListener {
             startActivity(Intent(this@MainActivity, main_page::class.java))
         }
+
+
+
     }
 }
