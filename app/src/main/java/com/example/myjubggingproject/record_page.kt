4366bc40ef13a73690240b278cbabcd049d18f78
wooandrich <brad1182@naver.com>
2022-11-05 16:06:31 +0900

    calendar

---
+++ app/src/main/java/com/example/myjubggingproject/record_page.kt
@@ -0,0 +1,11 @@
+package com.example.myjubggingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class record_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_page)
+    }
+}
