016e90990497c541fc5fa8013a270a04ad6fb41e
wooandrich <brad1182@naver.com>
2022-11-05 16:09:09 +0900

    calendar plus

--- app/src/main/java/com/example/myjubggingproject/main_page.kt
+++ app/src/main/java/com/example/myjubggingproject/main_page.kt
@@ -1,12 +1,23 @@
 package com.example.myjubggingproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myjubggingproject.databinding.ActivityMainBinding
+import com.example.myjubggingproject.databinding.ActivityMainPageBinding
 
 class main_page : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_page)
+        binding = ActivityMainPageBinding.inflate(layoutInflater)
+        setContentView(binding.root) //그릴 xml 뷰 파일을 연결 시켜준다.
+
+        // 기록 버튼을 누르면 화면 전환
+        binding.btnTimeImg.setOnClickListener {
+            startActivity(Intent(this@main_page, record_page::class.java))
+        }
     }
 }
