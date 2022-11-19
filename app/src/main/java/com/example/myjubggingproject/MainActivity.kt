7e7d8a494d45ccdd4623b135a4ce2983456328f2
wooandrich <brad1182@naver.com>
2022-11-20 01:35:18 +0900

    woomap

--- app/src/main/java/com/example/myjubggingproject/MainActivity.kt
+++ app/src/main/java/com/example/myjubggingproject/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.myjubggingproject
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.myjubggingproject.databinding.ActivityMainBinding
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,8 +16,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import com.example.myjubggingproject.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,18 +36,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root) //그릴 xml 뷰 파일을 연결 시켜준다.
 
         // intent main -> flogging
-        btn_floging.setOnClickListener {
-            supportFragmentManager.beginTransaction().run {
-                replace(binding.mainFragment.id, FloggingFragment())
-                commit()
-            }
+        binding.btnFloging.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
         }
+
 
         // 기록 이미지 -> recordActivity
         binding.btnTimeImg.setOnClickListener {
             val intent = Intent(this, RecordPageActivity::class.java)
             startActivity(intent)
         }
+
+
+
+
+
         // 날씨 구현
 
         //Create Retrofit Builder
