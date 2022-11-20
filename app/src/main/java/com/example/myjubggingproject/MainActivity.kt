241e97de2f55c998799594a0a45ba7f3661647ca
sebin <cntpqls0218@gmail.com>
2022-11-21 00:05:58 +0900

    pull woo map

--- app/src/main/java/com/example/myjubggingproject/MainActivity.kt
+++ app/src/main/java/com/example/myjubggingproject/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myjubggingproject
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.gson.annotations.SerializedName
@@ -14,9 +13,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import com.example.myjubggingproject.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,34 +24,28 @@ class MainActivity : AppCompatActivity() {
         var lon = "126.785823"
     }
 
-    private lateinit var auth: FirebaseAuth
     lateinit var binding: ActivityMainBinding
-   
-/*----------------------파이어베이스---------------------------------------------------------*/
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root) //그릴 xml 뷰 파일을 연결 시켜준다.
 
-        auth = Firebase.auth
-        auth.createUserWithEmailAndPassword("2020126109", "abcd")
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) { //로그인 성공
 
-                 Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-                } else {
-
-
-                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
-                }
+        // intent main -> flogging
+        btn_floging.setOnClickListener {
+            supportFragmentManager.beginTransaction().run {
+                replace(binding.mainFragment.id, FloggingFragment())
+                commit()
             }
+        }
 
- /*---------------------------------MainActivity -> FloggingFragment------------------------------*/
-  
-/*-------------------------기록 이미지 -> recordActivity -> 다시 Fragment ^^ !------------------------*/
-        
-/*-------------------------------------날씨 구현-----------------------------------------------------*/
+        // 기록 이미지 -> recordActivity
+        /*binding.btnTimeImg.setOnClickListener {
+            val intent = Intent(this, RecordPageActivity::class.java)
+            startActivity(intent)
+        }*/
+        // 날씨 구현
 
         //Create Retrofit Builder
         val retrofit = Retrofit.Builder()
@@ -78,19 +68,10 @@ class MainActivity : AppCompatActivity() {
                     val weatherResponse = response.body()
                     Log.d("MainActivity", "result: " + weatherResponse.toString())
 
-                    val name = weatherResponse!!.sys!!.name
-                    var icon = weatherResponse!!.weather!!.get(0).icon
-                    binding.tvName.text = name
-
-                    val country = weatherResponse!!.sys!!.country
-                    binding.tvCountry.text = country
-
-                    var mainWeather = weatherResponse!!.weather!!.get(0).main
-                    binding.tvMain.text = mainWeather
-
-
-                    var subWeather = weatherResponse!!.weather!!.get(0).description
-                    binding.tvDescription.text = subWeather
+                    binding.tvName.text = weatherResponse!!.sys!!.name
+                    binding.tvCountry.text = weatherResponse!!.sys!!.country
+                    binding.tvMain.text = weatherResponse!!.weather!!.get(0).main
+                    binding.tvDescription.text = weatherResponse!!.weather!!.get(0).description
 
                     var temp = weatherResponse!!.main!!.temp - 273.15
                     binding.tvTemp.text = String.format("%.1f °C", temp)
@@ -100,7 +81,7 @@ class MainActivity : AppCompatActivity() {
                         .load(
                             resources.getDrawable(
                                 resources.getIdentifier(
-                                    "icon_" + icon,
+                                    "icon_" + weatherResponse!!.weather!!.get(0).icon,
                                     "drawable",
                                     packageName
                                 )
@@ -152,7 +133,6 @@ class Sys {
     var name: String? = null
 
 }
-
 
 
 
