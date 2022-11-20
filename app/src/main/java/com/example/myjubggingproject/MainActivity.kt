0d8573417263c25f7cf06089db1df86a219b0095
sebin <cntpqls0218@gmail.com>
2022-11-21 00:47:53 +0900

    resolve conflict with woomap

--- app/src/main/java/com/example/myjubggingproject/MainActivity.kt
+++ app/src/main/java/com/example/myjubggingproject/MainActivity.kt
@@ -15,6 +15,7 @@ import retrofit2.http.Query
 import com.example.myjubggingproject.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     companion object{
